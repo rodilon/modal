e4f2828c06d78c238dccdd4cf473040683d1ed67
rodilon <renanodilon@gmail.com>
2020-01-05 21:04:24 -0300

    Modifica customView e cria alguns tratamentos; Cria um tipo para os botoes de filtro;

--- app/src/main/java/br/com/rodilon/testebancomodal/home/ButtonFilterView.kt
+++ app/src/main/java/br/com/rodilon/testebancomodal/home/ButtonFilterView.kt
@@ -1,12 +1,22 @@
 package br.com.rodilon.testebancomodal.home
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import br.com.rodilon.testebancomodal.R
+import br.com.rodilon.testebancomodal.home.TypeConstants.TYPE_CONST_FILTER
+import br.com.rodilon.testebancomodal.home.TypeConstants.TYPE_CONST_HOME
+import kotlinx.android.synthetic.main.view_button_filter.view.*
 
 class ButtonFilterView : FrameLayout {
+
+    private lateinit var container: ConstraintLayout
+    private lateinit var textView: AppCompatTextView
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -16,19 +26,47 @@ class ButtonFilterView : FrameLayout {
         defStyleAttr
     )
 
-    constructor(
-        context: Context,
-        attrs: AttributeSet?,
-        defStyleAttr: Int,
-        defStyleRes: Int
-    ) : super(context, attrs, defStyleAttr, defStyleRes)
-
     init {
         LayoutInflater.from(context).inflate(R.layout.view_button_filter, this, true)
     }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+        container = view_button_filter_container
+        textView = view_button_text
+    }
 
+    fun makeLayout(type: Int): ButtonFilterView {
+
+        when (type) {
+            TYPE_CONST_FILTER -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textView.setTextAppearance(R.style.TextAppearance_MontserratSemiBold_Black)
+            } else {
+                textView.setTextAppearance(context, R.style.TextAppearance_MontserratSemiBold_Black)
+            }
+            TYPE_CONST_HOME -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                textView.setTextAppearance(R.style.TextAppearance_ArialNarrowBoldItalic_Black)
+            } else {
+                textView.setTextAppearance(
+                    context,
+                    R.style.TextAppearance_ArialNarrowBoldItalic_Black
+                )
+            }
+        }
+        return this
+    }
+
+    fun setupContentButton(text: String): ButtonFilterView {
+        textView.text = text
+        return this
+    }
+
+    private fun deleteButton() {
+        textView.setOnClickListener { container.visibility = View.GONE }
+    }
+
+    fun build(): ButtonFilterView {
+        deleteButton()
+        return this
     }
 }
---
+++ app/src/main/java/br/com/rodilon/testebancomodal/home/TypeConstants.kt
@@ -0,0 +1,6 @@
+package br.com.rodilon.testebancomodal.home
+
+object TypeConstants {
+    const val TYPE_CONST_HOME = 1111
+    const val TYPE_CONST_FILTER = 2222
+}
