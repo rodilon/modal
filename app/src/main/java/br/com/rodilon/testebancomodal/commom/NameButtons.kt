837d509f7ee6a1388f5c11aad436f45531750a20
Renan Odilon <renan.o.oliveira@accenture.com>
2020-01-06 19:22:54 -0300

    Cria object para as labels dos botoes da customView

---
+++ app/src/main/java/br/com/rodilon/testebancomodal/commom/NameButtons.kt
@@ -0,0 +1,9 @@
+package br.com.rodilon.testebancomodal.commom
+
+object NameButtons {
+    const val NAME_FIRST_BUTTON = "DATA"
+    const val NAME_SECOND_BUTTON = "SEGUIDORES"
+    const val NAME_THIRD_BUTTON = "DECRESCENTE"
+    const val NAME_GROWING_BUTTON = "CRESCENTE"
+    const val NAME_STARS_BUTTON = "ESTRELAS"
+}
